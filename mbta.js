00e71fafd70c5a843e0d5805ff3c7b62e204f6cd
3zamil <3zamil@gmail.com>
2019-11-23 12:26:57 +0300

    Abdullah Alzamil Solution 1

---
+++ mbta.js
@@ -0,0 +1,63 @@
+const mbta = {
+  red: [
+    "South Station",
+    "Park Street",
+    "Kendall",
+    "Central",
+    "Harvard",
+    "Porter",
+    "Davis",
+    "Alewife"
+  ],
+  green: [
+    "Government Center",
+    "Park Street",
+    "Boylston",
+    "Arlington",
+    "Copley",
+    "Hynes",
+    "Kenmore"
+  ],
+  orange: [
+    "North Station",
+    "Haymarket",
+    "Park Street",
+    "State",
+    "Downtown Crossing",
+    "Chinatown",
+    "Back Bay",
+    "Forest Hills"
+  ]
+};
+
+const stopsBetweenStations = function(
+  startLine,
+  startStation,
+  endLine,
+  endStation
+) {
+  if (startLine === endLine) {
+    numOfStations = Math.abs(
+      mbta[startLine].indexOf(endStation) -
+        mbta[startLine].indexOf(startStation)
+    );
+  } else {
+    numOfStations =
+      Math.abs(
+        mbta[startLine].indexOf("Park Street") -
+          mbta[startLine].indexOf(startStation)
+      ) +
+      Math.abs(
+        mbta[endLine].indexOf(endStation) - mbta[endLine].indexOf("Park Street")
+      );
+  }
+  return Math.abs(numOfStations);
+};
+
+console.log(stopsBetweenStations("red", "Alewife", "red", "Alewife"));
+console.log(stopsBetweenStations("red", "South Station", "red", "Alewife"));
+console.log(stopsBetweenStations("red", "South Station", "green", "Kenmore"));
+console.log("\n");
+console.log(stopsBetweenStations("orange", "Back Bay", "green", "Kenmore"));
+console.log(stopsBetweenStations("red", "Davis", "red", "Harvard"));
+console.log(stopsBetweenStations("red", "South Station", "red", "Park Street"));
