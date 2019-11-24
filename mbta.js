036fbbef50461e0ac5a9301226e9c30fd3b1788d
Alsubaie89 <A_H_3_D_89@hotmail.com>
2019-11-24 09:03:06 +0300

    new hw 5

---
+++ mbta.js
@@ -0,0 +1,21 @@
+let allStations = {
+redLine : ["South station", "Park street","kendall", "Central", "Harvard","Porter", "Davis", "Alewife"],
+greenLine : ["Government Center", "Park street","Boylston","Arlington","Copley","Hynes","Kenmore"],
+orangeLine : ["North station","Hay market","Park street","State","Downtown crossing","Chinatown", "Back bay","Forest hill"],
+}
+let startLine = redLine
+let startStation = "South station";
+let endLine = redLine
+let endStation = "Alewife";
+
+
+
+
+
+
+
+
+
+//stopsBetweenStations("redLine","South station","redLine", "Alewife" )
+//let redLine = {}
+//console.log(allStations)
