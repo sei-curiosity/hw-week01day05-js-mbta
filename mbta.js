b8b3efd80f4a13095018aca71930d447fed4a08b
zanaalma <zanaalma1@gmail.com>
2019-11-24 08:42:17 +0300

    barely done

---
+++ mbta.js
@@ -0,0 +1,26 @@
+
+// calculate the number of stops between stations on the "MBTA" 
+let subwayline1 = {
+    red1:"southstation",
+    red2:"park street",
+    red3:"kendal",
+    red4:"central",
+    red5:"porter",
+    red6:"davis",
+    red:"alewife",
+    stopsBetweenStations: function(){
+
+        
+       
+        console.log(subwayline1.stopsBetweenStations 
+        ["StartLine" ,"StartStation","EndLine","EndStation"].reduce((a ,b ,c,d) => 
+        1+2+4+4))
+    }
+     
+    
+}
+        //i think gonna use an array and sum them and print them 
+        //do more search girl!!!
+
+
+    
